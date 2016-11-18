093e80362bbb31ceb20e2443c52d72ea96f2024a
[01;31m[K"name"[m[K: "Matthew Ramuta <[01;31m[K"email"[m[K: "matthew.g.ramuta@gmail.com>
2016-11-18 11:09:33 -0600

    finish session controller

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,3 +1,21 @@
 get '/sessions/new' do
 	erb :'sessions/new'
 end
+
+post '/sessions' do
+  if params[:email] && params[:pass]
+    params[:email]
+    user = User.authenticate(params[:email],params[:pass])
+  end
+  if user
+    session['user'] = user.id
+    redirect '/'
+  end
+  @error = "Invalid email/password combination."
+  redirect '/'
+end
+	
+delete '/sessions/new' do
+	sessions[:user] = nil
+	redirect '/'
+end
