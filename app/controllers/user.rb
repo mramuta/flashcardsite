1de75b8b5707f18c69ad44459818ccb570a3c866
travispbuckley <travispbuckley@lewisu.edu>
2016-11-18 11:50:38 -0600

    create routes for user controller

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,3 +1,14 @@
-get '/user/new' do 
+get '/users/new' do 
+	erb :"/users/new"
+end
+
+post '/users'
+	user = User.new[params[:user]]
+	if user.invalid?
+		@error = user.errors.full_message
+		return :"user/new"
+	end
+	user.save
+	redirect '/sessions/new'
 
 end
